b947ab53911060891661489cd5af20177c0600f5
nargiz-dev306 <3008nergiz@gmail.com>
2021-05-22 10:49:01 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const values = { "12ay":[0.11, 0.115],
+                  "24ay":[0.11, 0.115],
+                  "36ay":[0.11, 0.115],}
+console.log(values);
+  let tr= document.createElement("tr");
+  document.querySelector(".values").append(tr);
+  tr.className ="test";
