b5055509bf1d490ac776fafbd0501da32eafc3b5
nargiz-dev306 <3008nergiz@gmail.com>
2021-05-27 01:03:35 +0400

    I finish it

--- index.js
+++ index.js
@@ -2,42 +2,49 @@ let AYLIQ = "AYLIQ";
 let MUDDETIN_SONUNDA = "MUDDETIN_SONUNDA";
 let AZN = "AZN";
 let USD = "USD";
+let a=AYLIQ;
+let rows = "";
+let valyuta = "AZN";
+let mebleg = "50000";
+let muddet = "24"; 
+let umumiFaizGeliriOutput ='';
+let ayliqFaizGeliriOutput ='';
 const values = {
   12: {
-      AZN: {
-           AYLIQ: "0.11",
-           MUDDETIN_SONUNDA: "0.115",
-      },
-      USD: {
-          AYLIQ: "0.015",
-          MUDDETIN_SONUNDA: "0.02",
-      },
-  },   
+    AZN: {
+      AYLIQ: "0.11",
+      MUDDETIN_SONUNDA: "0.115",
+    },
+    USD: {
+      AYLIQ: "0.015",
+      MUDDETIN_SONUNDA: "0.02",
+    },
+  },
   24: {
-      AZN: {
-           AYLIQ: "0.11",
-           MUDDETIN_SONUNDA: "0.115",
-      },
-      USD: {
-          AYLIQ: "0.015",
-          MUDDETIN_SONUNDA: "0.02",
-      },
-  },  
+    AZN: {
+      AYLIQ: "0.11",
+      MUDDETIN_SONUNDA: "0.115",
+    },
+    USD: {
+      AYLIQ: "0.015",
+      MUDDETIN_SONUNDA: "0.02",
+    },
+  },
   36: {
-      AZN: {
-           AYLIQ: "0.11",
-           MUDDETIN_SONUNDA: "0.115",
-      },
-      USD: {
-          AYLIQ: "0.015",
-          MUDDETIN_SONUNDA: "0.02",
-      },
-  },  
-}
-let rows = "";
-const value =[12, 24, 36];
-for(let i=0; i<3; i++){
-rows += `
+    AZN: {
+      AYLIQ: "0.11",
+      MUDDETIN_SONUNDA: "0.115",
+    },
+    USD: {
+      AYLIQ: "0.015",
+      MUDDETIN_SONUNDA: "0.02",
+    },
+  },
+};
+let faiz = values[12]['AZN']['AYLIQ'];
+const value = [12, 24, 36];
+for (let i = 0; i < 3; i++) {
+  rows += `
   <tr>
     <td>${value[i]}ay</td>
     <td>${values[12][AZN][AYLIQ] * 100}%</td>
@@ -46,100 +53,94 @@ rows += `
     <td>${values[12][USD][MUDDETIN_SONUNDA] * 100}%</td>
   </tr>
 `;
-document.getElementById("tableBody").innerHTML = rows;
+  document.getElementById("tableBody").innerHTML = rows;
 }
-let faiz=``;
+
+  const faiziTap = () =>{
+    if (a===AYLIQ && valyuta===AZN) {
+      faiz = values[12]['AZN'][AYLIQ];
+    } 
+    else if (a===MUDDETIN_SONUNDA && valyuta===AZN){
+      faiz = values[12]['AZN'][MUDDETIN_SONUNDA];
+    }
+    else if (a===AYLIQ && valyuta===USD){
+      faiz = values[12]['USD'][AYLIQ];
+    }
+    else {
+      faiz = values[12]['USD'][MUDDETIN_SONUNDA];
+  }
+  };
+ 
+const umumiFaizGeliri = (muddet, mebleg, faiz) => { 
+  faiziTap();
+   umumiFaizGeliriOutput= (+mebleg * +muddet * +faiz);
+  document.querySelector(".umumiFaiz").innerHTML = umumiFaizGeliriOutput;
+};
+
+const ayliqFaizGeliri = (umumiFaizGeliriOutput, muddet) => {
+  ayliqFaizGeliriOutput = ( +umumiFaizGeliriOutput / +muddet);
+  document.querySelector(".ayliqFaiz").innerHTML = ayliqFaizGeliriOutput;
+};
+const faizDerecesi = (faiz) =>{
+  faiziTap();
+  faizDerecesiOutput = faiz * 100 +'%';
+  document.querySelector('.faizDerecesi').innerHTML = faizDerecesiOutput;
+}
+umumiFaizGeliri(mebleg, muddet, faiz);
+ayliqFaizGeliri(umumiFaizGeliriOutput, muddet);
+faizDerecesi(faiz);
+
 let checkbox = document.querySelector('input[type="checkbox"]');
-checkbox.addEventListener('change', function () {
+checkbox.addEventListener("change", function () {
   if (checkbox.checked) {
-   faiz= `${MUDDETIN_SONUNDA}` ;
-   document.getElementById("style").classList.add("hiddenItem");
+    a= MUDDETIN_SONUNDA;
+    document.getElementById("style").classList.add("hiddenItem");
   } else {
-    faiz= `${AYLIQ}` ;
-   document.getElementById("style").classList.remove("hiddenItem");
+   a= AYLIQ;
+    document.getElementById("style").classList.remove("hiddenItem");
   }
+  faiziTap();
+  umumiFaizGeliri(mebleg, muddet, faiz);
+  faizDerecesi(faiz);
+  ayliqFaizGeliri(umumiFaizGeliriOutput, muddet);
 });
-
-let valyuta ='';
- document.getElementById('valyuta').addEventListener('change', function() {
+document.getElementById("valyuta").addEventListener("change", function () {
   valyuta = this.value;
-  if (valyuta === USD){
-   AYLIQ = values[12][USD][AYLIQ];
-   MUDDETIN_SONUNDA = values[12][USD][MUDDETIN_SONUNDA];
+    if(valyuta===AZN){
+      valyuta=AZN;
+      document.querySelector('.currency-difference1').innerHTML= valyuta;
+      document.querySelector('.currency-difference2').innerHTML= valyuta;
+    }
+   else {
+    valyuta=USD;
+    document.querySelector('.currency-difference1').innerHTML= valyuta;
+    document.querySelector('.currency-difference2').innerHTML= valyuta;
   }
-  else{
-    AYLIQ = values[12][AZN][AYLIQ];
-    MUDDETIN_SONUNDA = values[12][AZN][MUDDETIN_SONUNDA];
-  }
+  faiziTap();
+  umumiFaizGeliri(mebleg, muddet, faiz);
+  ayliqFaizGeliri(umumiFaizGeliriOutput, muddet);
+  faizDerecesi(faiz);
 });
-let mebleg="50000";
+
 const amountRange = document.querySelector(".amountRange");
-const amountOutput = document.querySelector('#amount');
-amountRange.addEventListener('input', function (){
-   amountOutput.innerHTML = amountRange.value;
-   mebleg= amountRange.value;
+const amountOutput = document.querySelector("#amount");
+
+amountRange.addEventListener("input", function () {
+  amountOutput.innerHTML = amountRange.value;
+  mebleg = amountRange.value;
+  umumiFaizGeliri(mebleg, muddet, faiz);
+  ayliqFaizGeliri(umumiFaizGeliriOutput, muddet);
 });
-let muddet = "24";
+
 const periodRange = document.querySelector(".periodRange");
-const periodOutput = document.querySelector('#period');
-periodRange.addEventListener('input', function (){
-   periodOutput.innerHTML = periodRange.value;
-    muddet= periodRange.value;
+const periodOutput = document.querySelector("#period");
+
+periodRange.addEventListener("input", function () {
+  periodOutput.innerHTML = periodRange.value;
+  muddet = periodRange.value;
+  umumiFaizGeliri(mebleg, muddet, faiz);
+  ayliqFaizGeliri(umumiFaizGeliriOutput, muddet);
 });
-
-
-const umumiFaizGeliri = (muddet, mebleg, faiz) =>{
- console.log(+mebleg * +muddet * +faiz);
-}
-umumiFaizGeliri(mebleg,muddet,faiz);
-const ayliqFaizGeliri = (muddet, mebleg, faiz) =>{
-  return (+mebleg * +faiz/ +muddet);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let periodValue = document.getElementById("periodRangeInput");
-document.getElementById('periodRangeInput').addEventListener('input', function() {
-  periodCurrentValue = period.value;
-  console.log(periodCurrentValue);
-  console.log(period);
-});
-let amountCurrentValue = '';
-let  amountValue = document.getElementById("amountRangeInput");
-document.getElementById('amountRangeInput').addEventListener('input', function() {
-  amountCurrentValue = amount.value;
-  console.log(amountCurrentValue);
-});*/
-
 
 
 
